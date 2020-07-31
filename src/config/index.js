2c4649ce20242a5b6c656b715a898321f8f85e4a
Lucas-Moura-Da-Silva <lucas.silva566949@gmail.com>
2020-07-31 20:45:24 -0300

    ajuste do ajuste

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://cultoflix.herokuapp.com/categorias';
+  : 'https://cultoflix.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
